edcf35dec27ba1e4be10771e7ae43b6851d7bb96
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-03-12 22:10:20 -0400

    almost done

--- main.py
+++ main.py
@@ -1,25 +1,77 @@
 import random
 
-from art import LOGO
+from art import LOGO,RULES
+print(f"Our Game House Rules\n\n{RULES}")
 print(LOGO)
 
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
-player_cards = []
-sum_of_player = 0
-computer_cards = []
-sum_of_computer = 0
-br=0
-while(br<2):
-    player_start = random.choice(cards)
-    player_cards.append(player_start)
-    br+=1
+#function - random card generator form the list of cards
+def generateCard(cards_list):
+    return random.choice(cards_list)
+#function that gives computer new cards wof needed and calculates sum
+def computerTurn(cards_list, pc_cards, pc_sum):
+    while pc_sum<=16:
+        new_card = generateCard(cards_list)
+        pc_cards.append(new_card)
+        pc_sum += new_card
+    return pc_sum
 
-computer_start = random.choice(cards)
 
-computer_cards.append(computer_start)
-for card in player_cards:
-    sum_of_player = sum_of_player+card
+#gameplay
+ongoing_game = True
+while ongoing_game:
+    #set everything to the starting postions
+    player_cards = []
+    computer_cards = []
+    sum_of_player=0
+    sum_of_computer=0
 
-print(f"Your cards: {player_cards}, current score: {sum_of_player}")
-print(f"Computer's first card: {computer_start}")
+    play = input("Do you want to play Blackjack? Type 'y' or 'n': ").lower()
+    if play=='y':
+        br = 0
+        #generate 2 cards for player, and add to sum
+        while (br < 2):
+            player_card = generateCard(cards)
+            player_cards.append(player_card)
+            sum_of_player = sum_of_player + player_card
+            br += 1
+
+        #generate a card for PC and add to sum
+        computer_card = generateCard(cards)
+        sum_of_computer = sum_of_computer + computer_card
+        computer_cards.append(computer_card)
+
+    #if user typed n for playing a Blackjack terminate the program
+    elif play=='n':
+        break
+    else:
+        print("wrong input!")
+        break
+
+    print(f"Your cards: {player_cards}, current score: {sum_of_player}")
+    print(f"Computer's first card: {computer_cards}\n")
+
+#trace user to hit or stand and see who wins at the end
+    hit_or_stand = True
+    while hit_or_stand:
+        sum_of_player=21#temporary value for checking
+        if sum_of_player==21:
+            print("BLACKJACK!!!")
+            new_pc_sum = computerTurn(cards, computer_cards, sum_of_computer)
+            print(f"Your final hand: {player_cards}, final score: {sum_of_player}")
+            print(f"Computer's final hand: {computer_cards}, final score {new_pc_sum}")
+            if sum_of_player==new_pc_sum:
+                print('draw')
+            break
+#todo - finsh up hit and stand functionality
+        add_cards = input("Type 'h' to get another cards, type 's' to pass: ").lower()
+        if add_cards == 's':
+            break
+
+        new_card = generateCard(cards)
+        player_cards.append(new_card)
+        sum_of_player += new_card
+        print(f"Your cards: {player_cards}, current score: {sum_of_player}")
+        print(f"Computer's first card: {computer_cards}\n")
+
 
--- venv/art.py
+++ venv/art.py
@@ -6,3 +6,10 @@ LOGO = """.------.            _     _            _    _            _
 '-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_|
       |  \/ K|                            _/ |                
       '------'                           |__/"""
+RULES = """The deck is unlimited size
+- There are no jokers
+- The Jack/Quen/King all count as 10
+- The Ace can count as 11 or 1
+- The cards in the list will have equal probability of being drawn
+- Cards are not removed from the deck as they are drawn
+- The computer is the dealer"""
